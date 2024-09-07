8ff0a27ca241d54965250c868f2cca9f7038c687
Sebas L <isebs62@gmail.com>
2024-09-07 18:42:22 -0500

    Envio set de tenis. Sebastian Ladino

--- src/main/java/ejercicios/SetDeTenis.java
+++ src/main/java/ejercicios/SetDeTenis.java
@@ -7,9 +7,17 @@ import java.util.Scanner;
  * @author danielsanchez
  */
 public class SetDeTenis {
-    public static String evaluar(int numVictoriasA, int numVictoriasB) {
+    public static String evaluar(int numVictoriasA, int numVictoriasnB) {
         // TODO: Coloca aquí el código del ejercicio 1: Set de tenis
-        return "";
+            if (( numVictoriasA>= 6 && numVictoriasA - numVictoriasnB >= 2) || (numVictoriasA == 7 && numVictoriasnB == 6)) {
+            return "Ganó A";
+        } else if ((numVictoriasnB >= 6 && numVictoriasnB - numVictoriasA >= 2) || (numVictoriasnB == 7 && numVictoriasA == 6)) {
+            return "Ganó B";
+        } else if (numVictoriasA <= 6 && numVictoriasnB <= 6) {
+            return "Aún no termina";
+        } else {
+            return "Inválido";
+        }
     }
     
     public static void main(String[] args) {

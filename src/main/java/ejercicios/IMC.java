7ffd9dd1e67f635925d22e681cdae7909d5ddf0f
Sebas L <isebs62@gmail.com>
2024-09-07 18:40:18 -0500

    Envio IMC. Sebastian Ladino

--- src/main/java/ejercicios/IMC.java
+++ src/main/java/ejercicios/IMC.java
@@ -11,7 +11,16 @@ import java.util.Scanner;
 public class IMC {
     public static String evaluar(int peso, double estatura, int edad) {
         // TODO: Coloca aquí el código del ejercicio 8: Índice de masa corporal
-        return "";
+         double imc = peso / (estatura * estatura);
+        
+        if (imc < 18.5) {
+            return "bajo";
+        } else if (imc >= 18.5 && imc < 24.9) {
+            return "medio";
+        } else {
+            return "alto";
+            
+        }
     }
     
     public static void main(String[] args) {
